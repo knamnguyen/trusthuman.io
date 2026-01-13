36ba4dd6a11cbbd3af38aa2d433e9ac54867d3c2
Zi Hao LAM <lamzihao98@gmail.com>
2026-01-14 01:40:17 +0800

    clerk providers redirect based on source

--- apps/nextjs/src/lib/providers/providers.tsx
+++ apps/nextjs/src/lib/providers/providers.tsx
@@ -1,11 +1,11 @@
 import type { ReactNode } from "react";
-import { ClerkProvider } from "@clerk/nextjs";
 
 import { ThemeProvider } from "@sassy/ui/theme";
 
 import { WarmupProvider } from "~/app/_components/warmup-provider";
 import { LinkedInAccountProvider } from "~/stores/linkedin-account-store";
 import { TRPCReactProvider } from "~/trpc/react";
+import { EngagekitClerkProvider } from "./clerk-provider";
 
 export function Providers({ children }: { children: ReactNode }) {
   return (
@@ -13,12 +13,12 @@ export function Providers({ children }: { children: ReactNode }) {
       {/* important to have linkedIn account provider outside of TRPC provider */}
       <LinkedInAccountProvider>
         <TRPCReactProvider>
-          <ClerkProvider afterSignOutUrl="/" redirectUrl="/extension-auth">
+          <EngagekitClerkProvider>
             <WarmupProvider>
               {/* <ProgressBar height="4px" color="#155dfc" shallowRouting /> */}
               {children}
             </WarmupProvider>
-          </ClerkProvider>
+          </EngagekitClerkProvider>
         </TRPCReactProvider>
       </LinkedInAccountProvider>
     </ThemeProvider>
