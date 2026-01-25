d225f04e4f99a8aa85538413bbea3796ba863703
Zi Hao LAM <lamzihao98@gmail.com>
2026-01-25 18:54:14 +0800

    posthog proxy

--- apps/nextjs/src/instrumentation-client.ts
+++ apps/nextjs/src/instrumentation-client.ts
@@ -2,8 +2,6 @@ import posthog from "posthog-js";
 
 import { env } from "./env";
 
-console.info("wasup doing this shit now");
-
 posthog.init(env.NEXT_PUBLIC_POSTHOG_API_KEY, {
   api_host: "https://us.i.posthog.com",
   defaults: "2025-11-30",
